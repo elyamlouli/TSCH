4678917f5fbd50144eafa7ceaa994bc4a440dd5b
Tching Michel <michel.tching@hotmail.fr>
2022-04-20 17:15:35 +0200

    fix square.sh

--- scripts/square.sh
+++ scripts/square.sh
@@ -19,7 +19,7 @@ coordinator_node="38"
 mkdir -p $path
 
 name="$delai-$seed"
-res=$(iotlab-experiment submit -n square-$name -d 20 -l "strasbourg,m3,$sender_nodes,../firmwares/simple-sender-$name.iotlab" -l "strasbourg,m3,$coordinator_node,../src/coordinator.iotlab") 
+res=$(iotlab-experiment submit -n square-$name -d 20 -l "strasbourg,m3,$sender_nodes,../firmwares/simple-sender-$delai.iotlab" -l "strasbourg,m3,$coordinator_node,../src/coordinator.iotlab") 
 iotlab-experiment wait 
 id=$(echo "$res" | sed -n -e '2{p;q}' | cut -f2 -d":")
 serial_aggregator -i $id > $path/square-$name.txt
